b1fee36e92447fc90cf04b1b7bace0285f35ce85
student <jaflores33@miners.utep.edu>
2020-07-27 16:12:56 -0600

    led adjustments

--- project3/buttonMain.c
+++ project3/buttonMain.c
@@ -7,16 +7,18 @@
 #include <lcdutils.h>
 #include <lcddraw.h>
 
+//#define GREEN_LED BIT6
+
 void main(void)
 {
   configureClocks();
   switch_init();
-  led_init();
+  //led_init();
   buzzer_init();
 
   lcd_init();
   //clearScreen(COLOR_CYAN);
 
   
-  or_sr(0x18);  // CPU off, GIE on
+or_sr(0x18);  // CPU off, GIE on
 } 
